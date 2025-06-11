6ad367fc324dab35e86032874ecfcfd792f64d26
Daniel Kasambala <danielkasambala51@gmail.com>
2025-06-12 00:26:24 +0200

    fixing deployment

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME || DATABASE_URL, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT, 
     dialect: 'mysql', // or 'postgres', 'sqlite', etc. depending on your database
